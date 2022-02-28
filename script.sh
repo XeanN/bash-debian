243e1706388be4892dd5d334b40044680e8e7b6e
XeanN <angel.xp.pb@gmail.com>
2022-02-28 13:50:32 -0500

    actualizado

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@
 if [ ! -f /var/vagrant_provision-version1 ]; then
     # Instalar software necesario
     apt-get update
-    apt-get install -y nginx vim mariadb-server postgresql
+    apt-get install -y vim postgresql
 fi
